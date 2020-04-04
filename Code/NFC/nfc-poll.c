a6b24b873ff2390e996c959216b7d77bd26fa35f
Andrew Carroll <acarro12@its.jnj.com>
2020-04-04 12:18:49 -0400

    Adding custom nfc-poll.c

---
+++ Code/NFC/nfc-poll.c
@@ -0,0 +1,125 @@
+/*-
+ * Free/Libre Near Field Communication (NFC) library
+ *
+ * Libnfc historical contributors:
+ * Copyright (C) 2009      Roel Verdult
+ * Copyright (C) 2009-2013 Romuald Conty
+ * Copyright (C) 2010-2012 Romain Tartière
+ * Copyright (C) 2010-2013 Philippe Teuwen
+ * Copyright (C) 2012-2013 Ludovic Rousseau
+ * See AUTHORS file for a more comprehensive list of contributors.
+ * Additional contributors of this file:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  1) Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *  2 )Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Note that this license only applies on the examples, NFC library itself is under LGPL
+ *
+ */
+
+/**
+ * @file nfc-poll.c
+ * @brief Polling example
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif // HAVE_CONFIG_H
+
+#include <err.h>
+#include <inttypes.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <nfc/nfc.h>
+#include <nfc/nfc-types.h>
+
+#include "utils/nfc-utils.h"
+
+#define MAX_DEVICE_COUNT 16
+
+static nfc_device *pnd = NULL;
+static nfc_context *context;
+
+static void stop_polling(int sig)
+{
+  (void) sig;
+  if (pnd != NULL)
+    nfc_abort_command(pnd);
+  else {
+    nfc_exit(context);
+    exit(EXIT_FAILURE);
+  }
+}
+
+static void
+
+int
+main(int argc, const char *argv[])
+{
+
+  signal(SIGINT, stop_polling);
+
+  const uint8_t uiPollNr = 20;
+  const uint8_t uiPeriod = 2;
+  const nfc_modulation nmModulations[1] = {
+    { .nmt = NMT_ISO14443A, .nbr = NBR_106 },
+  };
+  const size_t szModulations = 1;
+
+  nfc_target nt;
+  int res = 0;
+
+  nfc_init(&context);
+
+  pnd = nfc_open(context, NULL);
+
+  if (pnd == NULL) {
+    ERR("%s", "Unable to open NFC device.");
+    nfc_exit(context);
+    exit(EXIT_FAILURE);
+  }
+
+  if (nfc_initiator_init(pnd) < 0) {
+    nfc_perror(pnd, "nfc_initiator_init");
+    nfc_close(pnd);
+    nfc_exit(context);
+    exit(EXIT_FAILURE);
+  }
+
+  if ((res = nfc_initiator_poll_target(pnd, nmModulations, szModulations, uiPollNr, uiPeriod, &nt))  < 0) {
+    nfc_perror(pnd, "nfc_initiator_poll_target");
+    nfc_close(pnd);
+    nfc_exit(context);
+    exit(EXIT_FAILURE);
+  }
+
+  if (res > 0) {
+    print_nfc_target(&nt, False);
+  } else {
+    printf("No target found.\n");
+  }
+  nfc_close(pnd);
+  nfc_exit(context);
+  exit(EXIT_SUCCESS);
+}
