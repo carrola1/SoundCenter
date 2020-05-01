c5b56cfc12ef01135ea4cfe1fe33fa115f032e40
carrola1 <carrola1@gmail.com>
2020-04-30 22:16:40 -0400

    Detecting rotary switches. Replaced bend with echo

--- Code/NFC/nfc_app.py
+++ Code/NFC/nfc_app.py
@@ -10,12 +10,21 @@ audio_path = '/home/pi/Documents/GitHub/SoundCenter/Audio/'
 song_dict = {'25b057c9': 'red.wav',
              '85c4546a': 'green.wav'} 
 
-led_pin = 13
+# Setup GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(led_pin, GPIO.OUT)
-GPIO.output(led_pin, GPIO.LOW)
 
+# Setup audio wake output
+wake_pin = 10
+GPIO.setup(wake_pin, GPIO.OUT)
+GPIO.output(wake_pin, GPIO.HIGH)
+
+# Setup bend rotary switch
+echo_sw_pins = [4, 17, 27, 22]
+for pin in echo_sw_pins:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Setup I2C bus and GPIO Expander
 i2cbus = smbus.SMBus(1)
 i2cbus.write_byte_data(0x44, 0x0D, 0xFF)  # set gpio expander inputs to have pull-ups
 
@@ -27,19 +36,21 @@ while(1):
 
   infile = song_dict.get(uid)
 
-  totalsec = 14 #pre-program this for each wav file
-
   if (infile is not None): 
 
     infile = audio_path + infile
 
-    GPIO.output(led_pin, GPIO.HIGH)
+    GPIO.output(wake_pin, GPIO.LOW)
 
-    pitch = (i2cbus.read_byte_data(0x44, 0x0F))^255 - 1
-
-    # 0-14 values
-    chorus = 0
-    bend = 0
+    exp_switches = i2cbus.read_byte_data(0x44, 0x0F)
+    chorus = (exp_switches >> 4)^0x0F - 1
+    pitch = (exp_switches & 0x0F)^0x0F - 1
+    echo = 0
+    pow2 = 0
+    for pin in echo_sw_pins:
+        echo = echo + (GPIO.input(pin)^1)*2**pow2
+        pow2 = pow2 + 1
+    echo = echo - 1
 
     # Translate Switch Inputs
     pitch_param = ['pitch', str((pitch-8)*100)]
@@ -52,24 +63,21 @@ while(1):
         else:
             chorus_param = chorus_param + ['{:.2f}'.format(x) for x in [(ii+1)*20, 0.3+0.03*(ii-2), 0.25+0.05*(ii-2), 2]] + ['-s']
 
-    # bend([delay, freq_shift, duration])
-    num_bends = max(int((bend+1)/2), 1) # up to 8 bends
-    bend_duration = (totalsec - 0.1)/num_bends/2
-    bend_param = ['gain', '-5', 'bend', '-f', '80', '-o', '4']
-    for ii in range(0, num_bends):
-        if (ii % 2 == 0):
-            bend_param = bend_param + [','.join(['{:.2f}'.format(x) for x in [(ii+1)*bend_duration/2, 300*(ii+1), bend_duration]])]
-            ','.join(bend_param)
-        else:
-            bend_param = bend_param + [','.join(['{:.2f}'.format(x) for x in [(ii+1)*bend_duration/2, -300*(ii+1), bend_duration]])]
+    # echo(gain-in, gain-out, [delay, decay])
+    echo_param = ['echo', '0.8', '0.9']
+    if (echo < 9):
+        echo_param = echo_param + ['{:.2f}'.format(x) for x in [echo*100, 0.4]]
+    else:
+        echo_param = echo_param + ['{:.2f}'.format(x) for x in [800, 0.4]]
+        echo_param = echo_param + ['{:.2f}'.format(x) for x in [echo*100, 0.3]]
 
-    cmd = ['play', infile]
+    cmd = ['play', infile, 'gain', '-5']
     if ((pitch < 7) | (pitch > 8)):
         cmd = cmd + pitch_param
     if (chorus > 0):
         cmd = cmd + chorus_param
-    if (bend > 0):
-        cmd = cmd + bend_param
+    if (echo > 0):
+        cmd = cmd + echo_param
     stdout, stderr = Popen(cmd, stdout=PIPE, stderr=PIPE).communicate()
 
-    GPIO.output(led_pin, GPIO.LOW)
+    GPIO.output(wake_pin, GPIO.HIGH)
